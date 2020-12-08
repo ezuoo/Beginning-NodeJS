d8e1db5a82a83e9fe2f5a9056b55e8dbb9fac503
ezuoo <juho0428@naver.com>
2020-12-08 22:20:10 +0900

    checkout getUserByID checkout deleteUserByID

--- app.js
+++ app.js
@@ -1,9 +1,80 @@
 const express = require('express');
 const app = express();
 
-app.get('/',(rep, res) => {
-    res.send('Hello World! ');
-})
+let users = [
+    {   id: 1, name: 'bae' },
+    {   id: 2, name: 'alice' },
+    {   id: 3, name: 'chris' }
+];
+
+var print = '';
+
+
+
+app.get('/', (rep, res) => {
+
+    res.json(users);
+});
+
+// get users
+app.get('/users/:id', (req, res) => {
+    const id = parseInt(req.params.id,10);
+    
+    if (!id) {
+        return res.status(400).json({error: 'Incorrect id'});
+      }
+    
+      let user = users.filter(user => user.id === id)[0]
+      
+      if(!user){
+          return res.status(404).json({ error: 'Unknown User !'});
+      }
+      
+      console.log(user);
+      var uu = new User(user.name);
+      uu.greeting().introduce();
+
+      return res.json(user);
+});
+
+app.delete('/users/:id', (req, res) => {
+
+    const id = parseInt(req.params.id,10);
+
+    if(!id) { return res.status(400).json({ error: 'Incorrect id !'}); }
+
+    const userIdx = users.findIndex( user => user.id === id );
+
+    console.log(`user Index : ${userIdx}`);
+
+    if(userIdx === -1) { return res.status(404).json({error: 'Unknown user'}); }
+    
+    users.splice(userIdx, 1);
+
+    res.status(204).send();
+
+});
+
+
+
+
+
 app.listen(3000, () => {
     console.log('Express app listening on port 3000 !');
 });
+
+
+
+
+function User(_name){
+    this.name = _name;
+}
+
+User.prototype.greeting = function(){
+    console.log('Hello!');
+    return this;
+};
+User.prototype.introduce = function() {
+    console.log(`I am ${this.name}`);
+    return this;
+};
